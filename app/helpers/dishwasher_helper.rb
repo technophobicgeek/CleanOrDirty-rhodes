2ab6a250b0a40aa5362c4ecbf04db641a775c3dd
Raj B <technophobicgeek@gmail.com>
2011-03-24 07:16:00 -0400

    working on fixing sync with asynchttp

--- app/helpers/dishwasher_helper.rb
+++ app/helpers/dishwasher_helper.rb
@@ -2,26 +2,18 @@ require 'json'
 
 module DishwasherHelper
   
-  @@server_url='http://192.168.15.8:3000/api/v1/dishwashers'
+  @@server_url='http://192.168.15.5:3000/api/v1/dishwashers'
   
   def change_status
     @dishwasher = Dishwasher.find(@params['id'])
-    status = @dishwasher.status
-    new_status = (status == 'dirty' ? 'clean' : 'dirty')
-
+    new_status = ( @dishwasher.status == 'dirty' ? 'clean' : 'dirty')
     @dishwasher.update_attributes({:status => new_status})
-    
-    # Sync:
-    #   update dishwasher on server
-    #   don't bother with response?
-    dishwasher_service_update
-    
+    dishwasher_service_update    
     render :action => :show
   end
 
   def create_dishwasher
     @dishwasher = Dishwasher.create(@params['dishwasher'])
-    # Sync: create dishwasher on server
     dishwasher_service_create
     render :action => :show
   end
@@ -70,31 +62,25 @@ module DishwasherHelper
   #   create dishwasher on server
   #   extract code from response
   #   update code in local db
-  def dishwasher_service_create()
+  def dishwasher_service_create
     Rho::AsyncHttp.post(
       :url => "#{@@server_url}",
       :body => ::JSON.generate({:name => @dishwasher.name}),
       :callback => url_for(:action => :dishwasher_create_callback)
     )
-    #render :action => :wait
   end
 
   def dishwasher_create_callback
     puts "dishwasher_create_callback: #{@params}"
     if @params['status'] != 'ok'
-      @@error_params = @params
       puts "Error in dishwasher_create_callback"
-      # TODO What do we do when no web connection?
     else
-      unless @dishwasher
-        @dishwasher = Dishwasher.find(:first)
-      end
-      code = @params['body']['code']
-      @dishwasher.update_attributes({:code => code})
+      @dishwasher = Dishwasher.find(:first)
+      @dishwasher.update_attributes({:code => @params['body']['code']})
     end
   end
 
-  def dishwasher_service_read()
+  def dishwasher_service_read
     Rho::AsyncHttp.get(
       :url => "#{@@server_url}/#{@dishwasher.code}",
       :callback => (url_for :action => :dishwasher_read_callback)
@@ -104,21 +90,26 @@ module DishwasherHelper
   def dishwasher_read_callback
     puts "dishwasher_read_callback: #{@params}"
     if @params['status'] != 'ok'
-      @@error_params = @params
       puts "Error in connection in dishwasher_read_callback"
-      # TODO What do we do when no web connection?
     else
-      unless @dishwasher
-        @dishwasher = Dishwasher.find(:first)
-      end      
-      status = @params['body']['status']  # should be ruby hash
-      puts "Returned status #{status}"
-      @dishwasher.update_attributes({:status => status}) # only if newer
-      #WebView.navigate(url_for :action => :show)
+      @dishwasher = Dishwasher.find(:first)
+      synchronize(@params['body'])
+    end
+  end
+  
+  def synchronize(sync_hash)
+    status = sync_hash['status']
+    puts "Returned status #{status}"
+    remote_update_TS = sync_hash['last_updated']
+    local_update_TS = @dishwasher.last_updated.to_i
+    if remote_update_TS > local_update_TS
+      @dishwasher.update_attributes({:status => status})
+    else
+      dishwasher_service_update
     end
   end
 
-  def dishwasher_service_update()
+  def dishwasher_service_update
     Rho::AsyncHttp.post(
       :url => "#{@@server_url}/update/#{@dishwasher.code}",
       :body => ::JSON.generate({:name => @dishwasher.name, :status => @dishwasher.status}),
@@ -126,7 +117,7 @@ module DishwasherHelper
     )
   end
 
-  def dishwasher_service_delete()
+  def dishwasher_service_delete
     Rho::AsyncHttp.post(
       :url => "#{@@server_url}/delete/#{@dishwasher.code}",
       :callback => url_for(:action => :dummy_callback)
